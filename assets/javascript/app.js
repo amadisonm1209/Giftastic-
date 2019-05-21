24ca832dc7893f493657a978e30d5400e9763998
Mads <madisonmartinez@Alicias-MacBook-Pro.local>
2019-05-21 16:36:35 -0600

    initial files

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+//exercise 10 from last week -- will bring up one image for one click
+//exercise 13 -- there's a for loop that will bring multiple gifs up 
+//exercise 15 -- pausing of the gif 
+//create an array of strings related to the topic of True Crime variable = topics
+//button click that will bring up 10 static gif images 
+//when the gif is clicked, it should animate
+//display the rating of the gif under each gif
+
