0a572953534e62a3cddd89a9e01b40ad4596e5d2
Mads <madisonmartinez@Alicias-MacBook-Pro.local>
2019-05-22 11:42:20 -0600

    gifs are pausing and animating

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -74,21 +74,21 @@ $(".crime-topic").on("click", function () {
 
         $("#gif-zone").prepend(gifDiv);
 
-        $(".gif").on("click", function () {
-
-          var state = $(this).attr("data-state");
-          console.log(state);
-
-          if (state === "still") {
-            $(this).attr("src", $(this).attr("data-animate"));
-            $(this).attr("data-state", "animate");
-          } else {
-            $(this).attr("src", $(this).attr("data-still"));
-            $(this).attr("data-state", "still");
-          }
-        });
       }
-    })
+
+      $(".gif").on("click", function () {
+
+        var state = $(this).attr("data-state");
+
+        if (state === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+        } else {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
+        }
+      });
+    });
 
 });
 
